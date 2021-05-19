6cd01bcb76a694fea33d7b485fe24c62e6c84d8e
user <# insert.html @app.route('/insert') def insertPage():     return render_template('insert.html')>
2021-05-19 21:11:14 +0800

    index.js欠json資料放到index.html顯示

--- static/script/index.js
+++ static/script/index.js
@@ -5,7 +5,8 @@ function view() {       //view() function
         jsonobj = r;        //將資料庫回傳值放到jsonobj
         //json 網頁資料呈現
 
-        //以下需要幫幫 用JS寫 把 json的資料塞到到index.html
+//        以下需要幫幫 用JS寫 把 json的資料塞到到index.html
+//         http://127.0.0.1:5000/allOrder/ <-這邊有整個find all的json資料
         console.log(jsonobj);//確認json
 
     });
