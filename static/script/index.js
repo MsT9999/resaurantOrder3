ca07261d74a6337821de8cc8b8d20e1bc2064a67
JieLin00 <50146lin@gmail.com>
2021-06-05 18:29:05 +0800

    修改難產

--- static/script/index.js
+++ static/script/index.js
@@ -1,1 +1,21 @@
 // 先放在index.html中，確認完全能使用在分檔
+const jsonUrl = "/allOrder/";
+var datas ;
+var counts;counts=0;
+function test(){
+        document.getElementById("order").innerHTML='';
+        var jsonobj = null  //空資料
+        $.getJSON(jsonUrl,function (r) {   //接受Main.py的資料庫回傳值
+        jsonobj = r;
+        console.log("json:",jsonobj);//確認json
+        data = jsonobj;
+         for (var item in data) {
+         console.log("id:",data[item]._id);
+           datas =  data[item].data._id;
+        }
+       });
+        counts=datas.length;
+         var i;
+         for (i=0;i < counts; i++) {
+         document.order.options[i] = new Option(datas[i],i); }
+        }
